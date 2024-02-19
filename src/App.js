d40d47877b9a04f5a9f378fd062249ba91bb8d9a
Santosh Suvarna <santosh@wundermanthompson.com>
2024-02-19 09:42:09 +0400

    Updated Bing URL and Key via config file

--- src/App.js
+++ src/App.js
@@ -1,28 +1,32 @@
-import React, { useState } from 'react';
-import SearchInput from './SearchInput';
-import SearchResults from './SearchResults';
-import './App.css';
+import React, { useState } from "react";
+import SearchInput from "./SearchInput";
+import SearchResults from "./SearchResults";
+import "./App.css";
+import config from "./config";
 
 function App() {
   const [results, setResults] = useState([]);
-
+  let bingUrl = config.api.url;
+  let bingKey = config.bing_api;
   const handleSearch = async (query) => {
     try {
-      const response = await fetch(`https://api.bing.microsoft.com/v7.0/search?q=${query}`, {
+      const response = await fetch(`${bingUrl}?q=${query}`, {
         headers: {
-          'Ocp-Apim-Subscription-Key': 'c83dc1e2fd8c461781e91960b9f32d38',
+          "Ocp-Apim-Subscription-Key": bingKey,
         },
       });
       const data = await response.json();
       setResults(data.webPages.value);
     } catch (error) {
-      console.error('Error fetching search results:', error);
+      console.error("Error fetching search results:", error);
     }
   };
 
   return (
     <div>
-      <div className='App-header'><SearchInput onSearch={handleSearch} /></div>
+      <div className="App-header">
+        <SearchInput onSearch={handleSearch} />
+      </div>
       <SearchResults results={results} />
     </div>
   );
